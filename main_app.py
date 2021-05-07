memoization added for flowfield computation, and point ordering fixed

main_app.py
@@ -4,6 +4,7 @@ import dash_html_components as html
 import dash_bootstrap_components as dbc
 import dash_daq as daq
 from dash.dependencies import Input, Output, State, ALL
+from flask_caching import Cache
 import plotly.graph_objs as go
 
 import pickle
@@ -21,7 +22,7 @@ ncores = cpu_count()
 # Load baseline aerofoil
 base_airfoil = pv.read('surface_base.vtk').points
 
-# Load baseline mesh (and subsample) - TODO load previously subsampled mesh, coeffs etc to save I/O
+# Load baseline mesh (and subsample)
 basegrid = pv.read('basegrid.vtk')
 xskip = 3
 yskip = 2
@@ -53,6 +54,8 @@ Y = np.load('Y.npy')[pts,:,:]
 ###################################################################
 # Define app
 app = dash.Dash(__name__, suppress_callback_exceptions=True,external_stylesheets=[dbc.themes.SPACELAB, 'https://codepen.io/chriddyp/pen/bWLwgP.css'])
+app.title = "Rapid flowfield estimation with polynomial ridges"
+cache = Cache(app.server, config={"CACHE_TYPE": "SimpleCache"})
 
 # Interface to define deformed airfoil via Hicks-Henne bumps 
 define_bumps = html.Div([
@@ -90,6 +93,15 @@ app.layout = dbc.Container(
     ],
     fluid = True
 )
+
+###################################################################
+# Function to compute flowfield
+###################################################################
+# This function is moderately time consuming so memoize it
+@cache.memoize(timeout=600)
+def compute_flowfield(design_vec):
+    ypred = np.array(Parallel(n_jobs=ncores,verbose=1,)(delayed(eval_poly)(design_vec,lowers[pt,var],uppers[pt,var],coeffs[pt,var,:],W[pt,var,:,:]) for pt in pts))
+    return ypred
 
 ###################################################################
 # Utilities to specify bumps and plot airfoils
@@ -212,30 +224,31 @@ def make_flowfield(n_clicks,airfoil_data,show_points):
     airfoil_y  = airfoil_data['airfoil-y']
     
     # Setup fig
-    layout={'clickmode':'event+select','margin':dict(t=10),'showlegend':False,"xaxis": {"title": 'x'}, "yaxis": {"title": 'y'}}
+    layout={'clickmode':'event+select','margin':dict(t=10),'showlegend':False,"xaxis": {"title": 'x'}, "yaxis": {"title": 'y'},
+            'paper_bgcolor':'white','plot_bgcolor':'white'}
 
     fig = go.Figure(layout=layout)
     fig.add_trace(go.Scatter(x=airfoil_x,y=airfoil_y,mode='lines',name='Deformed',line_width=8,line_color='blue',fill='tozeroy',fillcolor='rgba(0, 0, 255, 1.0)'))
 
     #fig.update_xaxes(range=[-1.12844, 1.830583])
-    fig.update_xaxes(range=[-0.8, 1.6])
-    fig.update_yaxes(range=[-0.5822106,0.5001755],scaleanchor = "x", scaleratio = 1)
+    fig.update_xaxes(range=[-0.8, 1.6], showgrid=False, zeroline=False, visible=False)
+    fig.update_yaxes(range=[-0.5822106,0.5001755],scaleanchor = "x", scaleratio = 1, showgrid=False, zeroline=False, visible=False)
 
-    # Contour plot (if button has been pressed)
+    # Contour plot (if button has just been pressed)
     changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
     if 'compute-flowfield' in changed_id:
-        ypred = np.array(Parallel(n_jobs=ncores,verbose=1,)(delayed(eval_poly)(design_vec,lowers[pt,var],uppers[pt,var],coeffs[pt,var,:],W[pt,var,:,:]) for pt in pts))
-        fig.add_trace(go.Contour(x=x,y=y,z=ypred.reshape(len(x),len(y)),transpose=True,contours=dict(
+        ypred = compute_flowfield(design_vec)
+        fig.add_trace(go.Contour(x=x,y=y,z=ypred.reshape(len(x),len(y)),transpose=True,colorbar=dict(title=var_name[var], titleside='right'), contours=dict(
             start=np.nanmin(ypred),
             end=np.nanmax(ypred),
             size=(np.nanmax(ypred)-np.nanmin(ypred))/20,
             )
         ))
 
-    if show_points: #TODO - need to seperate this into separate callback!
-        xx,yy = np.meshgrid(x,y)
+    if show_points: 
+        xx,yy = np.meshgrid(x,y,indexing='ij')
         fig.add_trace(go.Scatter(x=xx.flatten(),y=yy.flatten(),mode='markers',marker_color='black',opacity=0.4,marker_symbol='circle-open',marker_size=6,marker_line_width=2))
-
+       
     return fig, None
 
 ###################################################################
@@ -253,11 +266,13 @@ def display_click_data(clickData,airfoil_data):
         pointdata = clickData['points'][0]
         if "pointIndex" in pointdata: #check click event corresponds to the point cloud
             n = pointdata['pointIndex']
+            xn = pointdata['x']
+            yn = pointdata['y']
 
             # Plot training design
-            y = Y[n,:,var]
+            Yn = Y[n,:,var]
             u = (X @ W[pts][n,var,:,:]).flatten()
-            fig.add_trace(go.Scatter(x=u,y=y,mode='markers',name='Training designs',
+            fig.add_trace(go.Scatter(x=u,y=Yn,mode='markers',name='Training designs',
                 marker=dict(color='LightSkyBlue',size=15,opacity=0.5,line=dict(color='black',width=1))
             ))
 
@@ -271,15 +286,16 @@ def display_click_data(clickData,airfoil_data):
  
             # Plot poly
             u_poly = np.linspace(np.min(u)-0.25,np.max(u)+0.25,50)
-            y_poly = newpoly.get_polyfit(u_poly.reshape(-1,1))
-            fig.add_trace(go.Scatter(x=u_poly,y=y_poly.squeeze(),mode='lines',name='Ridge approximation',line_width=4,line_color='black' ))
+            Y_poly = newpoly.get_polyfit(u_poly.reshape(-1,1))
+            fig.add_trace(go.Scatter(x=u_poly,y=Y_poly.squeeze(),mode='lines',name='Ridge approximation',line_width=4,line_color='black' ))
 
             # Plot deformed design
             u_design = design_vec @ W[pts][n,var,:,:]
-            y_design = newpoly.get_polyfit(u_design)
-            fig.add_trace(go.Scatter(x=u_design.squeeze(),y=y_design.squeeze(),mode='markers',name='Deformed design',
+            Y_design = newpoly.get_polyfit(u_design)
+            fig.add_trace(go.Scatter(x=u_design.squeeze(),y=Y_design.squeeze(),mode='markers',name='Deformed design',
                 marker=dict(symbol='circle-open',color='firebrick',size=25,line=dict(width=5))
             ))
+            print(Y_design)
 
     return fig
 
@@ -287,8 +303,6 @@ if __name__ == '__main__':
     app.run_server(debug=True)
 
 # TODO:
-# 0) separate plotting points and flowfield into separate callbacks (so can toggle points without redoing flowfield) - will probs need to move fig declaration outside of make_flowback()
 # 1) dropdown to select var
-# 2) selected points aren't lining up
 # 3) Have figure below summary plot with W projected over aerofoil to tell user how to deform the aerofoil
 # 4) Tabs to switch between summary plot and inactive subspace