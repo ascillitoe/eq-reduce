c17d9fcd88dca883e6ec6e206705269905043c92
Ashley Scillitoe <ashleyscillitoe@googlemail.com>
2021-06-02 15:08:49 +0100

    Fix

--- main_app.py
+++ main_app.py
@@ -1,5 +1,4 @@
-i    airfoil_x  = np.hstack(airfoil_x[-1],airfoil_x)
-mport dash
+import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
