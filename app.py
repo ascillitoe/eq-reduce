e77f4e7efd74111123364414e97540a3987e9e18
Ashley Scillitoe <ashleyscillitoe@googlemail.com>
2021-06-09 15:39:11 +0100

    Trying to fix server

--- app.py
+++ app.py
@@ -7,3 +7,4 @@ app = dash.Dash(__name__, suppress_callback_exceptions=True,
 app.title = "Dimension reduction with equadratures"
 
 server = app.server
+server.secret_key = os.environ.get('secret_key', 'secret')
